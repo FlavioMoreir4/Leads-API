d5b53db2a2a25125a0e481687e3fd0fecae9e2b2
Flavio <flavio.moreira@outlook.com>
2020-11-14 16:50:54 -0300

    Iniciando Projeto

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,6 +35,24 @@ class LoginController extends Controller
 
         if ($this->attemptLogin($request)) {
             $user = $this->guard()->user();
+            if(!$user['api_token']){
+                //$user->generateToken();
+            }
+
+            return response()->json([
+                'data' => $user->toArray(),
+            ]);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function token(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->attemptLogin($request)) {
+            $user = $this->guard()->user();
             $user->generateToken();
 
             return response()->json([
